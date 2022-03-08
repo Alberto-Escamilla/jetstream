59c5d75bb20328400bd4d10c11ad8d1434b05945
Alberto-Escamilla <l161130082@gamadero.tecnm.mx>
2022-03-07 23:08:58 -0600

    jeatstream

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class HomeController extends Controller
+{
+    public function __invoke(){
+        // return view('home');
+        $role=Auth::user()->role;
+        if($role=='1'){
+            return view('admin.dashboard');
+        }else{
+            return view('dashboard');
+        }
+    }
+}
